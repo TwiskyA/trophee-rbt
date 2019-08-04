a4d1f527ea942f9c52655683694df6ced933b68b
TwiskyA <am-ti-lude@orange.fr>
2019-08-04 17:26:01 +0200

    first commit

---
+++ src/mainEvent/MainEvent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainInfo from './MainInfo';
+import './MainEvent.css';
+
+class MainEvent extends React.Component {
+  render() {
+    return (
+      <div className="main-event--wrapper">
+        <MainInfo />
+      </div>
+    );
+  }
+}
+
+export default MainEvent;
---
+++ src/mainEvent/MainInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MainInfo.css';
+
+class MainInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>3ème Trophée de Rambouillet</h1>
+        <div className="main-info--subtitles">
+          <h2>Pôle culturel de la Lanterne</h2>
+          <h2>26 Octobre 2019</h2>
+        </div>
+        <div className="main-info--subtitles">
+          <h3>prix</h3>
+          <h3>horaires</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainInfo;
