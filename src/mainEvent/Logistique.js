237ff3a227a574960602832aa96b28d20adadbad
TwiskyA <am-ti-lude@orange.fr>
2019-08-06 11:59:53 +0200

    Logistique

---
+++ src/mainEvent/Logistique.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Logistique extends React.Component{
+    render(){
+        return (
+        <div>
+            <ul>
+                <li>Restauration sur place</li>
+                <li>Places de parking à proximité mais <strong>pensez au covoiturage</strong></li>
+                <li>etc.</li>
+            </ul>
+        </div>
+        );
+    }
+}
+
+export default Logistique;
--- src/mainEvent/MainEvent.js
+++ src/mainEvent/MainEvent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainInfo from './MainInfo';
 import './MainEvent.css';
 import Program from './Program';
+import Logistique from './Logistique';
 
 class MainEvent extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class MainEvent extends React.Component {
       <div className="main-event--wrapper">
         <MainInfo />
         <Program />
+        <Logistique />
       </div>
     );
   }
